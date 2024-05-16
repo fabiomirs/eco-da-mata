7fd1b535ca5b242d3c29d9bb1e46143bda8c0a0a
Dollyn-boy <vitordoreahdx11@gmail.com>
2024-05-16 18:45:16 -0300

    modelos do mapa

---
+++ app_eco_da_mata/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppEcoDaMataConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_eco_da_mata'
---
+++ app_eco_da_mata/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Community(models.Model):
+    name = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models. FloatField()
+    description = models.TextField()
+    category = models.CharField(max_length=100, choices=(('COMMUNITY', 'Comunidade'),('TOURIST SPOT', 'Ponto Turístico')), default='COMMUNITY')
+    logo = models.ImageField()
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
+    text = models.TextField()
+    date = models.DateField()
+    category = models.CharField(max_length=100)
+    #on_delete = models.CASCADE deleta todas as notícias associadas às comunidades excluídas
+    community_key = models.ForeignKey(Community, on_delete=models.CASCADE, limit_choices_to={'category': 'COMMUNITY'})
