2ae2ba64b27b421f8d744b3fcc5d52306b0ec802
Matheus Felipe <matheusfelipesoares61@gmail.com>
2024-05-18 15:11:10 -0300

    ✅ Concluindo a aplicação de eventos
    A estrutura geral dos modelos de eventos e relatos foi concluída.

--- event_app/models.py
+++ event_app/models.py
@@ -6,7 +6,7 @@ class Event(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     time = models.TimeField()
-    description = models.TextField()
+    description = models.TextField(unique=True)
     class Category(models.TextChoices):
         LECTURE = 'LC', 'Lecture'
         FAIR = 'FR', 'Fair'
@@ -16,14 +16,17 @@ class Event(models.Model):
         ART_EXHIBITION = 'AE', 'Art Exhibition' 
         FESTIVAL = 'FV', 'Festival'
         OTHERS = 'OT', 'Others'
+    
     category = models.CharField(choices=Category.choices, default=Category.OTHERS)
-    adress = models.CharField(max_length=75, blank=True, null=True)
+    address = models.CharField(max_length=75, blank=True, null=True)
     location = models.CharField(max_length=50)
     link = models.URLField(blank=True, null=True)
     class Format(models.TextChoices):
         IN_PERSON = 'IP', 'IN_PERSON',
         ONLINE = 'ON', 'ONLINE',
         HYBRID = 'HB', 'HYBRID'
+
+    format = models.CharField(choices=Format.choices)
     value = models.DecimalField(default=0.00, max_digits=6, decimal_places=2)
     pix_key = models.CharField(max_length=77, blank=True, default='')
     pix_key_owner = models.CharField(max_length=50, blank=True, default='')
@@ -32,3 +35,17 @@ class Event(models.Model):
     questionary_link = models.URLField(blank=True, null=True, unique=True)
     profile_picture = models.ImageField(upload_to='images/events/profile_pictures')
     project_FK = models.ForeignKey(to="project_app.Project", on_delete = models.CASCADE) #Waiting project model to be defined
+
+
+class Review(models.Model):
+    person_name = models.CharField(max_length=50)
+    phrase = models.CharField(max_length=100)
+    class Grade(models.IntegerChoices):
+        GRADE_1 = 1
+        GRADE_2 = 2
+        GRADE_3 = 3
+        GRADE_4 = 4
+        GRADE_5 = 5
+    
+    grade = models.IntegerField(choices=Grade.choices)
+    event_FK = models.ForeignKey(to=Event, on_delete=models.CASCADE)
