6f089f4ca0058c5018e4d47f6fe7cc1a96549229
Marcelosgc1 <marcelobtsgc1@gmail.com>
2024-05-19 14:59:24 -0300

    :construction: app for 'project' initialized

--- eco_da_mata/settings.py
+++ eco_da_mata/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'event_app',
+    'project_app',
 ]
 
 MIDDLEWARE = [
---
+++ project_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    desctiption = models.CharField(max_length=800)
+    social_network_link = models.URLField(max_length=200)
+    telephone_number = models.CharField(max_length=20) # perhaps we can use 'django-phonenumber-field' extension.
+    email = models.EmailField(max_length=254)
+    #community_key = models.ForeignKey(to="community_app.Community", on_delete=models.CASCADE) # has to be implemented after those models been created
+    event_key = models.ForeignKey(to="event_app.Event", on_delete=models.CASCADE) # has to be implemented after those models been created
+
+    def __str__(self):
+        return self.name
+    
+class Management(models.Model):
+    project = models.ForeignKey(to=Project, on_delete=models.CASCADE)
+    #people = models.ForeignKey("people_app.People", on_delete=models.CASCADE) # has to be implemented after those models been created
+
