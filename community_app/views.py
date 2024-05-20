f3a6e94e81168c38a7ac9507a78003230ec5b4dc
Dollyn-boy <vitordoreahdx11@gmail.com>
2024-05-20 11:20:20 -0300

    atualização da aula

--- community_app/views.py
+++ community_app/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse
 from .models import Community, News
 
 # Create your views here.
-def communit_list(request):
+def communit_list(request): 
     communities = get_list_or_404(Community, category='COMMUNITY')
     return render(request, 'community_list.html', {'communities' : communities})
+
+def community_unique(request, id):
+    community = get_object_or_404(Community, pk=id)
+    return render(request, 'unique.html', {'community': community} )
