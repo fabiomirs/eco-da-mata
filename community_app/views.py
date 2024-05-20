aeebbaf38259998ed4d955a21fd5cb0f0139c976
Dollyn-boy <vitordoreahdx11@gmail.com>
2024-05-20 10:09:06 -0300

    views iniciais para comunidades

--- community_app/views.py
+++ community_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from django.http import HttpResponse
+from .models import Community, News
 
 # Create your views here.
+def communit_list(request):
+    communities = get_list_or_404(Community, category='COMMUNITY')
+    return render(request, 'community_list.html', {'communities' : communities})
