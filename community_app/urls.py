fdef0da95e869d0a2bf47a18855c69168f6fd6c7
Douglas Jesus <dougecomp@gmail.com>
2024-05-20 11:33:26 -0300

    :bug: fix: corrigindo url de unique view

--- community_app/urls.py
+++ community_app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import communit_list
+from .views import community_list, community_unique
 
 urlpatterns = [
-    path('', communit_list, name="communit_list"),
+    path('/', community_list, name="communit_list"),
+    path('<int:id>/', community_unique, name="communit_list"),
 ]
--- community_app/views.py
+++ community_app/views.py
@@ -3,10 +3,11 @@ from django.http import HttpResponse
 from .models import Community, News
 
 # Create your views here.
-def communit_list(request): 
+def community_list(request): 
     communities = get_list_or_404(Community, category='COMMUNITY')
     return render(request, 'community_list.html', {'communities' : communities})
 
 def community_unique(request, id):
     community = get_object_or_404(Community, pk=id)
+    print(community.id, community.name)
     return render(request, 'unique.html', {'community': community} )
