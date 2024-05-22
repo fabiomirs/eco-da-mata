2a00ffb74a83804c04bd91a180518fcd4874502f
Tamilly costa <Tamillycosta899@gmail.com>
2024-05-22 10:20:28 -0300

    add people model

---
+++ people_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from subcategory_app.models import Subcategory
+
+
+    
+class People(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=800)
+    institutional_email = models.EmailField(max_length=100)
+    personal_page_link = models.URLField(max_length=200)
+    #logo = models.ImageField(upload_to='people_logos/', blank=True)
+    category = models.CharField(max_length=20, choices=[('institution', 'Institution'), ('physical person', 'Physical person')])
+    subcategory_key = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    
+
+
+class Login(models.Model):
+    name = models.CharField(max_length=100)
+    senha = models.CharField(max_length=128) 
+    people_fk = models.ForeignKey('People', on_delete=models.CASCADE)
