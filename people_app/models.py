80d37bb82c099ff6a55c2f9c56f4ffedd223967c
Tamilly costa <Tamillycosta899@gmail.com>
2024-05-22 23:47:01 -0300

    refazendo atribitos em people e subcategoria

--- people_app/models.py
+++ people_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from subcategory_app.models import Subcategory
+#from subcategory_app.models import Subcategory
 
 
     
@@ -10,11 +10,15 @@ class People(models.Model):
     personal_page_link = models.URLField(max_length=200)
     #logo = models.ImageField(upload_to='people_logos/', blank=True)
     category = models.CharField(max_length=20, choices=[('institution', 'Institution'), ('physical person', 'Physical person')])
-    subcategory_key = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    #subcategory_key = models.ForeignKey(Subcategory, on_delete=models.CASCADE,default=1)
     
+    class Meta:
+        ordering = ['name']
 
+    def __str__(self):
+        return self.name
 
 class Login(models.Model):
     name = models.CharField(max_length=100)
-    senha = models.CharField(max_length=128) 
-    people_fk = models.ForeignKey('People', on_delete=models.CASCADE)
+    password = models.CharField(max_length=128) 
+    people_fk = models.ForeignKey('People', on_delete=models.CASCADE,default=1)
--- subcategory_app/models.py
+++ subcategory_app/models.py
@@ -6,3 +6,8 @@ class Subcategory(models.Model):
     title = models.CharField(max_length=50)
     category = models.CharField(max_length=20, choices=[('institution', 'Institution'), ('physical person', 'Physical person')])
 
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
