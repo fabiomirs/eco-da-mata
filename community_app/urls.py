59b01ba909b13d041531d66b306126d299db922d
fabiomirs <fabiomirs@users.noreply.github.com>
2024-05-23 13:18:02 -0300

    :bug: Problema no cadastro pelo formulário.
    - Implementação do formulário para create.

--- community_app/urls.py
+++ community_app/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import registred_communities, single_community
+from .views import registred_communities, single_community, CommunityCreate, NewsCreate
 
 urlpatterns = [
-    path('', registred_communities, name="registred_communities"),
+    path('listar/', registred_communities, name="registred_communities"),
+    path('criar/comunidade/', CommunityCreate.as_view(), name="communit_create"),
+    path('criar/noticia/', NewsCreate.as_view(), name="news_create"),
     path('<int:id>', single_community, name='single_community' )
 ]
--- community_app/views.py
+++ community_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse
 from .models import Community, News
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 def registred_communities(request): 
@@ -11,3 +13,18 @@ def single_community(request, id):
     community = get_object_or_404(Community, pk=id)
     news = News.objects.filter(community_key=id)
     return render(request, 'single_community.html', {'community': community, 'news': news} )
+
+
+class CommunityCreate(CreateView):
+    model = Community
+    fields = ["name", "link", "latitude", "longitude", "description",
+              "category", "logo"]
+    template_name = 'cadastros/formulario.html'
+    success_url = reverse_lazy('comunidade/')
+
+class NewsCreate(CreateView):
+    model = News
+    fields = ["title", "link", "text", "date",
+              "category"]
+    template_name = 'cadastros/formulario.html'
+    success_url = reverse_lazy('comunidade/')
