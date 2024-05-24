6f715db8b7565c7500d3067ed6b3022061a6537e
Marcelosgc1 <marcelobtsgc1@gmail.com>
2024-05-24 15:46:06 -0300

    :sparkles: feat: funcao para mostrar cada projeto individualmente

--- project_app/urls.py
+++ project_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_all_projects
+from .views import *
 
 urlpatterns = [
     path('', get_all_projects),
+    path('<int:pk>', get_single_project)
 ]
--- project_app/views.py
+++ project_app/views.py
@@ -6,3 +6,7 @@ from .models import Project
 def get_all_projects(request):
     projects = Project.objects.all()
     return render(request, 'projects.html', context={'projects':projects})
+
+def get_single_project(request, pk):
+    project = Project.objects.get(id=pk)
+    return render(request, 'single_project.html', context={'project':project})
