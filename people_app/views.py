a3d4a5132e755487e5d05db2224b6d84f4705949
Tamilly costa <Tamillycosta899@gmail.com>
2024-05-25 18:44:58 -0300

    ♻️  altera a estrutura da view para modelo baseado em classes

--- people_app/views.py
+++ people_app/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
-
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import People
+from django.views import View
 
 
-def people_list(request):
-    people_list = get_list_or_404(People)
-    return render(request, 'people_list.html', {'people_list': people_list})
+class PeopleList(View):
+    def get(self, request):
+        people_list = get_list_or_404(People)
+        
+        return render(request, 'people_list.html', {'people_list': people_list})
 
 
+class PeopleDetails(View):
+    def get(request, pk):
+        people = get_object_or_404(People, pk=pk)
+        
+        context = {"name": people.name,
+                   "description": people.description,
+                   "institutional_email": people.institutional_email,
+                   "personal_page_link": people.personal_page_link,
+                   "category": people.category
+                   }
+        return render(request, 'people_detail.html', context)
+    
+    
+
+    
