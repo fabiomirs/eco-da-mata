f76d41a14e29ae648608112c588c29344fecc0c9
Matheus Felipe <matheusfelipesoares61@gmail.com>
2024-05-25 18:59:49 -0300

    :card_file_box: raw: Inclusão das urls de eventos

--- eco_da_mata/urls.py
+++ eco_da_mata/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('comunidade/', include('community_app.urls')),
-    path('projetos/', include('project_app.urls'))
+    path('projetos/', include('project_app.urls')),
+    path('eventos/', include('event_app.urls')),
 ]
