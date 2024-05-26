8346491abf1e5437c669ed902c2f9c6297af3aef
Matheus Felipe <matheusfelipesoares61@gmail.com>
2024-05-26 17:30:35 -0300

    :card_file_box: raw: Requisição de atualização de eventos

--- eco_da_mata/urls.py
+++ eco_da_mata/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('comunidade/', include('community_app.urls')),
     path('projetos/', include('project_app.urls')),
-    path('eventos/', include('event_app.urls')),
+    path('events/', include('event_app.urls')),
 ]
