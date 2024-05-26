1ab32d0d2f12e07aabcaf59d6fc6a863ba572c32
Tamilly costa <Tamillycosta899@gmail.com>
2024-05-26 11:37:44 -0300

    ✨ feat: urls de deleção e atualização de pessoas

--- people_app/urls.py
+++ people_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import PeopleList, PeopleDetails, PeopleCreate
+from .views import PeopleList, PeopleDetails, PeopleCreate, PeopleUpdate, PeopleDelete
 
 
 urlpatterns = [
     path('people/', PeopleList.as_view(), name = 'people_list'),
-    path('<int:pk>/', PeopleDetails.as_view(), name='people_detail'),
+    path('people/<int:pk>/', PeopleDetails.as_view(), name='people_detail'),
     path('criar/', PeopleCreate.as_view(), name = 'people_regiser'),
+    path('atualizar/<int:pk>/', PeopleUpdate.as_view(), name = 'people_update'),
+    path('delete/<int:pk>/', PeopleDelete.as_view(), name = 'people_delete')
 ]
