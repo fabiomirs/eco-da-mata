8e6acc816015a742c25539e9abba6bfe36d67d02
Matheus Felipe <matheusfelipesoares61@gmail.com>
2024-05-26 15:04:25 -0300

    :sparkles: feat: Visualização para registro de eventos

--- event_app/urls.py
+++ event_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import EventList, DetailedEvent
+from .views import EventList, DetailedEvent, EventCreation
 
 urlpatterns = [
-    path('', EventList.as_view(), name='event-list'),
-    path('<int:event_id>/', DetailedEvent.as_view(), name='detailed-event'),
+    path('all/', EventList.as_view(), name='event-list'),
+    path('all/<int:event_id>/', DetailedEvent.as_view(), name='detailed-event'),
+    path('create', EventCreation.as_view(), name='create-event'),
 ]
--- event_app/views.py
+++ event_app/views.py
@@ -1,6 +1,7 @@
 from django.views import View
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Event
+from .forms import EventCreationForm
 
 # Create your views here.
 class EventList(ListView):
@@ -12,8 +13,17 @@ class EventList(ListView):
     def get_queryset(self):
         return Event.objects.all().order_by('start_date')
 
+
 class DetailedEvent(DetailView):
     model = Event
     template_name = 'detailed_event.html'  
     context_object_name = 'event'          
     pk_url_kwarg = 'event_id'
+
+
+class EventCreation(CreateView):
+    model = Event
+    form_class = EventCreationForm
+    template_name = 'event_creation.html'
+    success_url = '/eventos/all/'
+
