148e25a7e9de43d92297f44f515f7ef32bac1cbe
Guilherme Fernandes <143087629+DrizinCoder@users.noreply.github.com>
2024-05-26 17:04:43 -0300

    ✨ feat: cria opcao para atualizar projetos

--- project_app/urls.py
+++ project_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', get_all_projects, name='all_projects'),
     path('<int:pk>', get_single_project, name='single_project'),
     path('criar/', create_project, name='new_project'),
-    path('apagar/<int:pk>', delete_project, name='delete_project')
+    path('apagar/<int:pk>', delete_project, name='delete_project'),
+    path('atualizar/<int:pk>', update_project, name='update_project')
 ]
