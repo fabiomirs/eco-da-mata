99ded70cd91916a60fc168cd12b6642431cf5542
fabiomirs <141861499+fabiomirs@users.noreply.github.com>
2024-05-26 16:40:06 -0300

    :sparkles: feat: adiciona urls para atualizar e detalhar noticia.

--- community_app/urls.py
+++ community_app/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import registred_communities, single_community, community_create, community_delete,news_create,community_update
+from .views import registred_communities, single_community,news_update,news_detail, community_create, community_delete,news_create,community_update
 
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
     path('deletar/<int:id>', community_delete, name="delete_community"),
     path('atualizar/<int:id>/', community_update, name='update_community'),
     path('noticia/criar', news_create, name='create_news' ),
+    path('noticia/atualizar/<int:id>', news_update, name='update_news' ),
+    path('noticia/visualizar/<int:id>', news_detail, name='detail_news' ),
     path('<int:id>', single_community, name='single_community' ) # Entrada de dados da URL -> VIEW
 ]
