1d6dec66bff24a60218e4a891d18623c0c0b9ea0
Matheus Felipe <matheusfelipesoares61@gmail.com>
2024-05-26 21:09:01 -0300

    :broom: cleanup: Atualização das URLS

--- event_app/urls.py
+++ event_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import EventList, DetailedEvent, EventCreation, EventUpdate
+from .views import EventList, DetailedEvent, EventCreation, EventUpdate, EventDeletion
 
 urlpatterns = [
     path('all/', EventList.as_view(), name='event-list'),
-    path('all/<int:pk>/', DetailedEvent.as_view(), name='detailed-event'),
+    path('<int:pk>/', DetailedEvent.as_view(), name='detailed-event'),
     path('create/', EventCreation.as_view(), name='create-event'),
-    path('update/<int:pk>/', EventUpdate.as_view(), name='update-event')
+    path('<int:pk>/update/', EventUpdate.as_view(), name='update-event'),
+    path('<int:pk>/delete/', EventDeletion.as_view(), name='delete-event'),
 ]
