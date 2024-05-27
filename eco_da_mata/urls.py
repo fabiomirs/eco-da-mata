b073b300079ab76e870312a0a881a85648bc4f7b
Tamilly costa <Tamillycosta899@gmail.com>
2024-05-26 22:12:39 -0300

    ✨ feat: Urls de pessoas e subcategoria

--- eco_da_mata/urls.py
+++ eco_da_mata/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('comunidade/', include('community_app.urls')),
     path('projetos/', include('project_app.urls')),
     path('events/', include('event_app.urls')),
+    path('pessoas/',include('people_app.urls')),
+    path('subcategoria',include('subcategory_app.urls'))
 ]
