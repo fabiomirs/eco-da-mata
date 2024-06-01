5412f7e36b4d2532e1e797d63df04a71fdca282e
fabiomirs <fabiomirs@users.noreply.github.com>
2024-06-01 20:01:04 -0300

    :ok_hand: style: altera caminho para community.

--- eco_da_mata/urls.py
+++ eco_da_mata/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('comunidade/', include('community_app.urls')),
+    path('community/', include('community_app.urls')),
     path('projetos/', include('project_app.urls')),
     path('events/', include('event_app.urls')),
     path('pessoas/',include('people_app.urls')),
