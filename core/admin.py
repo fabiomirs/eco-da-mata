dba90e99d8722fef940aa034f553357732f3a82c
Guilherme Fernandes <143087629+DrizinCoder@users.noreply.github.com>
2024-06-09 15:38:34 -0300

    ✨feat: adicionando imagens a eventos

--- core/admin.py
+++ core/admin.py
@@ -9,20 +9,25 @@ class ImagemInline(admin.TabularInline):
     model = Imagem
     extra = 1
 
+
 class CommunityAdmin(admin.ModelAdmin):
     inlines = [ImagemInline]
-'''
+
+
 class EventAdmin(admin.ModelAdmin):
     inlines = [ImagemInline]
 
+
+'''
 class PeopleAdmin(admin.ModelAdmin):
     inlines = [ImagemInline]'''
+
 
 class ProjectAdmin(admin.ModelAdmin):
     inlines = [ImagemInline]
 
-admin.site.register(Community, CommunityAdmin)
 
-#admin.site.register(Event, EventAdmin)
-#admin.site.register(People, PeopleAdmin)
+admin.site.register(Community, CommunityAdmin)
+admin.site.register(Event, EventAdmin)
 admin.site.register(Project, ProjectAdmin)
+#admin.site.register(People, PeopleAdmin)
