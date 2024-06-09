c87ed704bd826cd561983252a358218d7d445cbc
Marcelosgc1 <marcelobtsgc1@gmail.com>
2024-06-09 11:18:15 -0300

    :construction: adiciona configuracao global de paginacao

--- eco_da_mata/settings.py
+++ eco_da_mata/settings.py
@@ -134,3 +134,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Rest framework
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
