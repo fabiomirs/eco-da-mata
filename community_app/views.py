2f8dde156103bf9a25d20459fbe6929b1758e65b
fabiomirs <fabiomirs@users.noreply.github.com>
2024-06-10 10:31:05 -0300

    :boom: fix: retorna ViewSet de community_app para ReadOnlyViewSet.

--- community_app/views.py
+++ community_app/views.py
@@ -9,11 +9,11 @@ from django.forms import modelformset_factory
 from .serializers import CommunitySerializer, NewsSerializer
 from rest_framework import viewsets
 
-class CommunityViewSet(viewsets.ModelViewSet):  
+class CommunityViewSet(viewsets.ReadOnlyModelViewSet):  
     queryset = Community.objects.all()
     serializer_class = CommunitySerializer
 
-class NewsViewSet(viewsets.ModelViewSet):
+class NewsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
 
