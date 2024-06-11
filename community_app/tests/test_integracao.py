7014766273b68c039b8abcbf387b55c5b6a06444
Dollyn-boy <vitordoreahdx11@gmail.com>
2024-06-11 19:22:19 -0300

    :white_check_mark:	test: Criação de testes unitários e de integração para comunidade.

--- community_app/tests/test_integracao.py
+++ community_app/tests/test_integracao.py
@@ -1,34 +1,30 @@
 from django.test import TestCase
 from django.test.client import Client
 from django.urls import reverse
+from rest_framework import status
 from community_app.models import Community
+from community_app.serializers import CommunitySerializer
 from datetime import date
 
 class CommunityIntegrationTestCase(TestCase):
     def setUp(self):
         self.client = Client()
-        Community.objects.create(
-            name = 'Comunidade de Teste',
-            link = "https://maps.app.goo.gl/pahfS5uhEWR2XJN97",
-            latitude = -12.588229716430375,
-            longitude = -55.739644976925305,
-            description = 'Descrição da comunidade de teste',
-            category ='COMMUNITY',
-            logo = None
+        self.community = Community.objects.create(
+            name='Comunidade de Teste',
+            link="https://maps.app.goo.gl/pahfS5uhEWR2XJN97",
+            latitude=-12.588229716430375,
+            longitude=-55.739644976925305,
+            description='Descrição da comunidade de teste',
+            category='COMMUNITY',
+            logo=None
         )
 
-        def test_ler_evento_via_get(self):
-            url = reverse('api/community')
-            response = self.clien.get(url,{
-                'name' : 'Comunidade de Teste',
-                'link' : "https://maps.app.goo.gl/pahfS5uhEWR2XJN97",
-                'latitude' : -12.588229716430375,
-                'longitude' : -55.739644976925305,
-                'description' : 'Descrição da comunidade de teste',
-                'category' :'COMMUNITY',
-                'logo' : None
-            })
+    def test_ler_comunidade_via_get(self):
+        url = reverse('community-detail', kwargs={'pk': self.community.pk})
+        response = self.client.get(url)
 
-            self.assertEqual(response.status_code, 200)
-            nova_comunidade = Community.objects.get(nome='Comunidade de Teste')
-            self.assertIsNotNone(nova_comunidade)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Verifique se os dados retornados correspondem aos dados da comunidade criada
+        serializer = CommunitySerializer(self.community)
+        self.assertEqual(response.data, serializer.data)
