6e584299b60906e11169cc234560119999abdd84
Tamilly costa <Tamillycosta899@gmail.com>
2024-06-12 22:41:35 -0300

    🗑️ remove aplicativo de subcategoria

--- subcategory_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SubcategoryAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'subcategory_app'
--- subcategory_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Subcategory
-
-class SubcategorySerializers(serializers.ModelSerializer):
-    class Meta():
-        model = Subcategory
-        fields = '__all__'
