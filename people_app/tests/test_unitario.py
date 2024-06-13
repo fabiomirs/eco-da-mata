12ddd356793b64b6cbaf16b814ea98a1f8f398e7
Tamilly costa <Tamillycosta899@gmail.com>
2024-06-12 23:51:18 -0300

    ✅ Cria testes unitários de subcategoria

--- people_app/tests/test_unitario.py
+++ people_app/tests/test_unitario.py
@@ -3,7 +3,10 @@ from people_app.models import People, Subcategory
 
 
 class PeopleTestCase(TestCase):
+  
+    
     def setUp(self):
+        subcategory = Subcategory.objects.create(title='Subcategoria de teste')
         People.objects.create(
             name = 'Parceiro1',
             description = 'Parceiro eco-da-mata',
@@ -11,15 +14,16 @@ class PeopleTestCase(TestCase):
             personal_page_link = 'http://linktest',
             logo = None,
             category = 'Instituição',
+            subcategory_key = subcategory
         )
     
         
-    def test_people_create_sucess(self):
+    def test_people_create_success(self):
         people = People.objects.get(name = 'Parceiro1')
         self.assertEqual(people.description,'Parceiro eco-da-mata' )
         
         
-    def test_nome_comunidade_nao_vazio(self):
+    def test_people_name(self):
         people = People.objects.get(name='Parceiro1')
         self.assertNotEqual(people.name, '')
         self.assertIsNotNone(people.name)
@@ -36,3 +40,23 @@ class PeopleTestCase(TestCase):
         
         
         
+class SubcategoryTestCase(TestCase):
+    def setUp(self):
+        People.objects.create(
+        title = 'teste subcategoria',
+        category = 'Pessoa fisica'
+        )
+        
+        
+    def test_subcategory_name(self):
+        subcategory = Subcategory.objects.get(title='teste subcategoria')
+        self.assertNotEqual(subcategory.title, '')
+        self.assertIsNotNone(subcategory.title)
+        
+        
+    def test_subcategory_category(self):
+        subcategory= Subcategory.objects.get(title='teste subcategoria')
+        self.assertIn(subcategory.category,['Instituição', 'Pessoa Fisica'])
+    
+        
+    
